2c58d38a67a8b732d51ed02acd158431c6ad36ac
ordinaraviro <ordinaraviro@gmail.com>
2024-07-10 17:31:13 +0300

    feat: add router

--- class-components/src/App.tsx
+++ class-components/src/App.tsx
@@ -1,13 +1,30 @@
 import "./App.css";
 import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
 import Main from "./Pages/Main/Main";
+import { Routes, Route } from "react-router-dom";
+import Page404 from "./Pages/Page404/Page404";
 
 function App() {
   return (
     <>
-      <ErrorBoundary>
-        <Main />
-      </ErrorBoundary>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ErrorBoundary>
+              <Main />
+            </ErrorBoundary>
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <ErrorBoundary>
+              <Page404 />
+            </ErrorBoundary>
+          }
+        />
+      </Routes>
     </>
   );
 }
