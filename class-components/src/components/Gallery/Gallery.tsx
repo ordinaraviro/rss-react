8f549b60c032c26a68b846b4289ef01b3d9e59f8
ordinaraviro <ordinaraviro@gmail.com>
2024-07-10 15:14:27 +0300

    feat: add gallery func component

--- class-components/src/components/Gallery/Gallery.tsx
+++ class-components/src/components/Gallery/Gallery.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { fetchData, BooksResponse } from "../../api/api";
 import "./Gallery.scss";
 
@@ -7,75 +7,54 @@ interface Props {
   perPage: number;
 }
 
-interface State {
-  data: BooksResponse | null;
-}
+const Gallery = (props: Props) => {
+  const [data, setData] = useState<BooksResponse | null>(null);
 
-class Gallery extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      data: null,
+  useEffect(() => {
+    setData(null);
+    const fetchDataAsync = async () => {
+      const result = await fetchData(props.searchText, props.perPage);
+      setData(result);
     };
-    this.fetchData = this.fetchData.bind(this);
+
+    fetchDataAsync();
+  }, [props.searchText, props.perPage]);
+
+  if (!data) {
+    return <div className="loading">Loading...</div>;
   }
 
-  async componentDidMount() {
-    this.fetchData();
+  if (!data.numFound) {
+    return <div className="loading">Nothing found</div>;
   }
 
-  async componentDidUpdate(prevProps: Props) {
-    if (prevProps.searchText !== this.props.searchText) {
-      this.fetchData();
-    }
-  }
+  const books = data.docs;
 
-  async fetchData() {
-    this.setState({ data: null });
-    const { searchText, perPage } = this.props;
-    const data = await fetchData(searchText, perPage);
-    this.setState({ data });
-  }
+  return (
+    <div className="gallery">
+      {books.map((book, index) => (
+        <div className="card" key={index}>
+          {book.cover_edition_key ? (
+            <img
+              src={`https://covers.openlibrary.org/b/olid/${book.cover_edition_key}-M.jpg`}
+              alt={book.title}
+            />
+          ) : (
+            <div className="no-img-title">{book.title}</div>
+          )}
 
-  render() {
-    const { data } = this.state;
-
-    if (!data) {
-      return <div className="loading">Loading...</div>;
-    }
-
-    if (!data.numFound) {
-      return <div className="loading">Nothing found</div>;
-    }
-
-    const books = data.docs;
-
-    return (
-      <div className="gallery">
-        {books.map((book, index) => (
-          <div className="card" key={index}>
-            {book.cover_edition_key ? (
-              <img
-                src={`https://covers.openlibrary.org/b/olid/${book.cover_edition_key}-M.jpg`}
-                alt={book.title}
-              />
-            ) : (
-              <div className="no-img-title">{book.title}</div>
-            )}
-
-            <div className="card-body">
-              <h5 className="card-title">{book.title}</h5>
-              <p className="card-text">
-                <small className="text-muted">
-                  {book.author_name ? book.author_name[0] : "Unknown"}
-                </small>
-              </p>
-            </div>
+          <div className="card-body">
+            <h5 className="card-title">{book.title}</h5>
+            <p className="card-text">
+              <small className="text-muted">
+                {book.author_name ? book.author_name[0] : "Unknown"}
+              </small>
+            </p>
           </div>
-        ))}
-      </div>
-    );
-  }
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Gallery;
