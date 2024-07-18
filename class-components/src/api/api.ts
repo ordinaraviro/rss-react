608d182e74517992023f7c7845816a9abb00d3ca
ordinaraviro <ordinaraviro@gmail.com>
2024-07-18 10:38:45 +0300

    fix: update api request

--- class-components/src/api/api.ts
+++ class-components/src/api/api.ts
@@ -1,22 +1,10 @@
 export interface BookInfo {
-  author_key: string[];
   author_name: string[];
   cover_edition_key: string;
   edition_key: string[];
-  ebook_access: string;
   first_publish_year: number;
   first_sentence: string[];
-  lending_edition_s: string;
-  publish_date: string[];
-  publisher: string[];
-  seed: string[];
   title: string;
-  title_suggest: string;
-  title_sort: string;
-  type: string;
-  subject: string[];
-  time: string[];
-  ratings_average: number;
 }
 
 interface BooksResponse {
@@ -36,7 +24,7 @@ async function fetchData(
   if (!searchText) searchText = "publish_year%2024";
   if (!pageNum) pageNum = "1";
   const response = await fetch(
-    `https://openlibrary.org/search.json?q=${searchText}&page=${pageNum}&limit=${perPage}`,
+    `https://openlibrary.org/search.json?q=${searchText}&page=${pageNum}&limit=${perPage}&fields=title,author_name,cover_edition_key,edition_key,first_publish_year,first_sentence`,
   );
   const data = await response.json();
   return data;
