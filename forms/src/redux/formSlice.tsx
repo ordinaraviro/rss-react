379c8d2855b59df365a227904d7fa59e03ca3f98
ordinaraviro <ordinaraviro@gmail.com>
2024-08-15 15:09:34 +0300

    feat: add redux

---
+++ forms/src/redux/formSlice.tsx
@@ -0,0 +1,36 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface FormData {
+  name: string;
+  age: number;
+  email: string;
+  password: string;
+  gender: string;
+  terms: boolean;
+  picture: string;
+  country: string;
+}
+
+interface FormState {
+  data: FormData[];
+  countries: string[];
+}
+
+const initialState: FormState = {
+  data: [],
+  countries: ["USA", "Canada", "UK"], 
+};
+
+const formSlice = createSlice({
+  name: "form",
+  initialState,
+  reducers: {
+    addFormData: (state, action: PayloadAction<FormData>) => {
+      state.data.push(action.payload);
+    },
+  },
+});
+
+export const { addFormData } = formSlice.actions;
+
+export default formSlice.reducer;
