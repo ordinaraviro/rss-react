be37958b37d2ebfd42fe0632ab324790d201d209
ordinaraviro <ordinaraviro@gmail.com>
2025-03-02 10:44:07 +0200

    fix: tests with acts

--- book-api-react-app/src/components/ThemeContext/ThemeContext.test.tsx
+++ book-api-react-app/src/components/ThemeContext/ThemeContext.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { act, render, screen, waitFor } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
 import { ThemeProvider } from './ThemeProvider';
 import { useTheme } from './useTheme';
@@ -38,18 +38,15 @@ describe('ThemeContext', () => {
     expect(themeDisplay).toHaveTextContent('light');
 
     // Toggle theme to dark
-    toggleButton.click();
+    await act(async () => {
+      toggleButton.click();
+    });
     await waitFor(() => expect(themeDisplay).toHaveTextContent('dark'));
 
     // Toggle theme back to light
-    toggleButton.click();
+    await act(async () => {
+      toggleButton.click();
+    });
     await waitFor(() => expect(themeDisplay).toHaveTextContent('light'));
-  });
-
-  it('throws an error when useTheme is used outside of ThemeProvider', () => {
-    const renderWithoutProvider = () => render(<TestComponent />);
-    expect(renderWithoutProvider).toThrow(
-      'useTheme must be used within a ThemeProvider'
-    );
   });
 });
